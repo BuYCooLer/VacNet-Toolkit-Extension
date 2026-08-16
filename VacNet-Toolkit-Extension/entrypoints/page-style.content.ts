f9d0ff39e18c07ddfd82db3f1825e9801194e08a
MartinDawgor <167915347+MartinDawgor@users.noreply.github.com>
2026-08-16 23:42:23 +0400

    Delete VacNet-Toolkit-Extension directory

--- VacNet-Toolkit-Extension/entrypoints/page-style.content.ts
+++
@@ -1,8 +0,0 @@
-import { defineContentScript } from 'wxt/utils/define-content-script';
-import '../src/page/page-style.css';
-
-export default defineContentScript({
-  matches: ['https://www.counter-strike.net/vacnet/clips*'],
-  runAt: 'document_start',
-  main() {},
-});
